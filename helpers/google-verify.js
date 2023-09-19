bfd32c73cfc1afb5f51b587a6b654d34ecad5b97
Juliettesm <l19160036@eldorado.tecnm.mx>
2023-09-19 11:46:11 -0700

    test

--- helpers/google-verify.js
+++ helpers/google-verify.js
@@ -3,6 +3,8 @@ const {OAuth2Client} = require('google-auth-library');
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 async function googleVerify( token = '') {
+
+  console.log("cliente_ID",process.env.GOOGLE_CLIENT_ID)
   const ticket = await client.verifyIdToken({
       idToken: token,
       audience: process.env.GOOGLE_CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
